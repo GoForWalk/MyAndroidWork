14d2cacd2828b10ed2c633788eb703505782128d
kit-lec <atcgsp912@gmail.com>
2020-04-14 10:44:07 +0900

    집 커밋2

--- a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
+++ a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear3);
+
+
+        int ss = 100;
+        String tt = "hello";
+        // 저장이랑 commit 이랑 개념이 다르다!!
+        // 변경 내용은 파란색으로 표시된다!!
+
+        
     }
 }
