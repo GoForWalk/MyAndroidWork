70d3ec5f8aa06b7bbf68a498b7c57f1b38691afa
kit-lec <atcgsp912@gmail.com>
2020-04-14 11:05:03 +0900

    학원커밋3-1

--- a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
+++ a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         int ss = 100;
-        String tt = "hello";
+        String tt = "hello 히히";
         // 저장이랑 commit 이랑 개념이 다르다!!
         // 변경 내용은 파란색으로 표시된다!!
 
